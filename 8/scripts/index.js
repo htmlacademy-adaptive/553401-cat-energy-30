d01df537158496f8057a7898afa542bbc44efe36
keksobot <keksobot@users.noreply.github.com>
2024-01-16 08:11:46 +0000

    Deploying to gh-pages from @ htmlacademy-adaptive/553401-cat-energy-30@5f276e77aa3cc58eb94cddd138ae1b8654638d0b 🚀

--- 8/scripts/index.js
+++
@@ -1,1 +0,0 @@
-var e=document.querySelector(".page-header__menu"),t=e.querySelector(".page-header__toggle"),n=document.querySelector(".contacts__map");e.classList.remove("page-header__menu--no-js");n.classList.remove("contacts__map--no-js");t.addEventListener("click",s=>{s.preventDefault(),e.classList.toggle("is-open"),t.classList.toggle("is-open")});
